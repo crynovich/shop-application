1e449afbe351781cd29685ccf01294677117d227
Marko Krajinović <marko.krajinovic.work@gmail.com>
2024-10-27 15:46:24 +0100

    add app navigation

--- src/app/navigation-bar/navigation-bar.tsx
+++ src/app/navigation-bar/navigation-bar.tsx
@@ -1,19 +1,40 @@
-import { AppBar, Badge, IconButton, Toolbar, Typography } from '@mui/material';
+import {
+  AppBar,
+  Badge,
+  Button,
+  IconButton,
+  Stack,
+  Toolbar,
+} from '@mui/material';
 import ShoppingCart from '@mui/icons-material/ShoppingCart';
+import { useNavigate } from 'react-router-dom';
+import { useCallback } from 'react';
 
 export function NavigationBar() {
+  const navigate = useNavigate();
+
+  const handleAppClick = useCallback(() => {
+    navigate('/');
+  }, [navigate]);
+
   return (
     <AppBar position="static">
       <Toolbar>
-        <Typography variant="h6" className="flex-1">
-          Shop Application
-        </Typography>
+        <Stack
+          className="w-full"
+          direction="row"
+          justifyContent="space-between"
+        >
+          <Button color="inherit" onClick={handleAppClick}>
+            Shop Application
+          </Button>
 
-        <IconButton size="large" color="inherit">
-          <Badge badgeContent={17} color="secondary">
-            <ShoppingCart />
-          </Badge>
-        </IconButton>
+          <IconButton size="large" color="inherit">
+            <Badge badgeContent={17} color="secondary">
+              <ShoppingCart />
+            </Badge>
+          </IconButton>
+        </Stack>
       </Toolbar>
     </AppBar>
   );
